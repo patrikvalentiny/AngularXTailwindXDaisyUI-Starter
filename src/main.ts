52f4db39a0ac0fe81fb49e08f1fe4be5e2e145d5
patrikvalentiny <patrikvalentiny@gmail.com>
2024-01-20 22:19:59 +0100

    update packages and move to standalone component

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule)]
+})
   .catch(err => console.error(err));
